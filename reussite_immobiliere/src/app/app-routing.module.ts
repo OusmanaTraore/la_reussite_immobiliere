4a406cd3a337472a1f4a4df7c7c6eae587222fcb
OusmanaTraore <traoreosman@yahooo.fr>
2021-03-22 23:34:25 +0100

    accueil

--- reussite_immobiliere/src/app/app-routing.module.ts
+++ reussite_immobiliere/src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'slider',
+    loadChildren: () => import('./slider/slider.module').then( m => m.SliderPageModule)
+  },
 ];
 
 @NgModule({
